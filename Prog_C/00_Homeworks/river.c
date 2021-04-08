49fba62e13cf16e072fc3604b2cbf024f76e93e7
DESKTOP-O4P5641\daxoj <daxoja@gmail.com>
2021-04-08 02:15:43 +0200

    Task_9

--- Prog_C/00_Homeworks/river.c
+++ Prog_C/00_Homeworks/river.c
@@ -3,6 +3,37 @@
 
 int main() 
 {   
+    FILE *fr;
+
+    int i=0; 
+    int pocet_kam = 0;
+    int *arr;
+
+    fr = fopen("C:/Users/daxoj/Dev/devSchool/Prog_C/00_Homeworks/data.txt", "r");
+    fscanf(fr, "%d", &pocet_kam);
     
+    // Alokovanie
+    arr = (int*)malloc(pocet_kam * sizeof(int));
+    for(i = 0; i<=pocet_kam; i++){
+        fscanf(fr, "%d ", &arr[i]);
+    }
+
+    // Porovnávanie "intervalovy_strom"
+    int days = 0, min = 0, sum = 0, pom;
+
+    for(i = 0; i < pocet_kam-1; i++) {
+        if(i == 0) min = arr[i] + arr[i+1];
+        sum = arr[i] + arr[i+1];
+        if(sum < min) {
+            min = sum;
+            pom = i;
+        }
+    }
+
+    days = (arr[pom] > arr[pom+1]) ? arr[pom] : arr[pom+1];
+    printf("%d", days);
+
+    fclose(fr);
+
     return 0;
 }
