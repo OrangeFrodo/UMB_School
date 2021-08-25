0c28b1860419da7a7aa10760b2b645635c475c58
OrangeFrodo <daxoja@gmail.com>
2021-08-25 22:34:55 +0200

    25_TaskFix

---
+++ Prog_C++/Intro/IntroV2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x = 5;
+    int y = 23;
+
+    cout << x;
+    cout << y;
+
+    return 0;
+}
---
+++ Prog_C++/ProjectV1/Networking/SimpleSocket.hpp
@@ -0,0 +1,28 @@
+#ifndef hdelibc_hpp
+#define hdelibc_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+namespace hde
+{
+    class SimpleSocket
+    {
+    private:
+        /* data */
+    public:
+        SimpleSocket(/* args */);
+        ~SimpleSocket();
+    };
+    
+    SimpleSocket::SimpleSocket(/* args */)
+    {
+    }
+    
+    SimpleSocket::~SimpleSocket()
+    {
+    }
+    
+}
+
+#endif 
