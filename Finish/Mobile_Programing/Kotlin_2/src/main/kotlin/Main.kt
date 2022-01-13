93cf95816acfb67cd2338ac18022ad95885682f9
OrangeFrodo <daxoja@gmail.com>
2022-01-13 14:18:19 +0200

    Excersises loops

---
+++ Finish/Mobile_Programing/Kotlin_2/src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+// Kotlin: conditions, functions, loops
+import excersise_one
+import excersise_two
+
+fun main(args: Array<String>) {
+    // Intro
+
+    /*
+    // Variables
+    val num: Int = 4;
+    val x: Boolean = num < 0 || num > 2
+    val y: Int? = readLine()?.toInt()
+
+    print("${x} \n")
+
+    // If statement
+    if (num < 0) {
+        println("Negative num")
+    } else if (num == 0) {
+        println("Zero")
+    } else {
+        println("Positive num")
+    }
+
+    // Switch
+    when(x){
+        false -> println("False")
+        true -> {
+            println("True")
+            println("Nice")
+        }
+    }
+
+    // Switch with return to varable
+    val text: String = when(y) {
+        1, 2 -> "One and two"
+        else -> "Not 1 nor 2"
+    }
+    */
+
+    // excersise_one()
+    excersise_two()
+
+}
