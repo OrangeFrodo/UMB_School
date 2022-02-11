2628f8dbb08a0d6159ddfd6b4b5618356b664435
OrangeFrodo <daxoja@gmail.com>
2022-02-11 10:36:33 +0200

    Drawable, Icons, Top and Bottom navs

--- Finish/Mobile_Programing/Kotlin_Week_5/app/src/main/java/com/example/week5/MainActivity.kt
+++ Finish/Mobile_Programing/Kotlin_Week_5/app/src/main/java/com/example/week5/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.week4test
+package com.example.week5
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,14 +10,34 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.week4test.ui.theme.Week4TestTheme
+import com.example.week5.ui.theme.Week5Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyView()
+            Week5Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MainView()
+                }
+            }
         }
     }
 }
 
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Week5Theme {
+        Greeting("Android")
+    }
+}
